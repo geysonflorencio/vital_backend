f3eaf9bd00ea72536dddeadb12449456ac0ec23d
Geyson S Florêncio <156634144+geysonflorencio@users.noreply.github.com>
2025-08-07 19:29:47 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,11 +3,16 @@ const apiRoutes = require('../routes');
 const authRoutes = require('../routes/auth');
 const { errorHandler, notFoundHandler } = require('../middleware/errorHandler');
 
+// Cria o app Express
 const { app } = createServer();
 
+// Rotas da API
 app.use('/api', apiRoutes);
 app.use('/api', authRoutes);
+
+// Middlewares de erro
 app.use(notFoundHandler);
 app.use(errorHandler);
 
+// Exporta o app para o Vercel
 module.exports = app;
