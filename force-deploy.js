91c5c4d4a403e9f940f1828c5596f45988bff98a
Geyson S Florêncio <156634144+geysonflorencio@users.noreply.github.com>
2025-08-08 19:54:52 -0300

    Delete force-deploy.js

--- force-deploy.js
+++
@@ -1,8 +0,0 @@
-// FORÇA REDEPLOY - Timestamp: 2025-08-08 19:30
-// Este arquivo força o Vercel a reconhecer as mudanças
-// Email configurado corretamente
-module.exports = {
-  deployTimestamp: new Date().toISOString(),
-  status: 'forcing_redeploy_with_correct_author',
-  author: 'geysonflorencio@gmail.com'
-};
