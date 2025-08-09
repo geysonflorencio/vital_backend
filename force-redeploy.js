fbaab38138fc91445051d1f7224b9026de4bb042
Geyson S Florêncio <156634144+geysonflorencio@users.noreply.github.com>
2025-08-08 23:23:01 -0300

    Delete force-redeploy.js

--- force-redeploy.js
+++
@@ -1,7 +0,0 @@
-// Force redeploy timestamp: 2025-08-08 21:16
-// This file forces Vercel to redeploy
-module.exports = {
-  timestamp: new Date().toISOString(),
-  purpose: 'force_redeploy',
-  issue: 'vercel_not_deploying_changes'
-};
